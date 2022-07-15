cecd7ed2be137c4dda93690a4ca86f9a98a539dd
Luca Pipolo <luca@somoscuatro.es>
2022-07-15 13:21:00 +0200

    Added prettier.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
+
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      'light-cornflower-blue': "#8ECAE6",
+      'light-cornflower-blue': '#8ECAE6',
       'light-cornflower-blue-200': 'rgba(142, 202, 230, 0.20)',
-      'blue-green': "#219EBC",
+      'blue-green': '#219EBC',
       'prussian-blue': '#023047',
       'selective-yellow': '#FFB703',
-      'tangerine': '#FB8500',
+      tangerine: '#FB8500',
       'tangerine-200': 'rgba(251, 133, 0, 0.20)',
-      'coconut': '#FFF',
+      coconut: '#FFF',
     },
     fontFamily: {
-      'sans': ['Jost', ...defaultTheme.fontFamily.sans],
+      sans: ['Jost', ...defaultTheme.fontFamily.sans],
     },
     fontSize: {
       sm: ['14px', '150%'],
@@ -37,7 +36,7 @@ module.exports = {
         xl: '984px',
         '2xl': '1152px',
       },
-    }
+    },
   },
   plugins: [],
-}
+};
