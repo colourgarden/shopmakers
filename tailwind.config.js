0266231cef69967ea4f0fd6a08935a992192944a
Luca Pipolo <luca@somoscuatro.es>
2022-07-15 13:10:53 +0200

    Added custom fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
@@ -16,6 +18,17 @@ module.exports = {
       'tangerine': '#FB8500',
       'tangerine-200': 'rgba(251, 133, 0, 0.20)',
       'coconut': '#FFF',
+    },
+    fontFamily: {
+      'sans': ['Jost', ...defaultTheme.fontFamily.sans],
+    },
+    fontSize: {
+      sm: ['14px', '150%'],
+      base: ['16px', '150%'],
+      lg: ['18px', '150%'],
+      xl: ['24px', '150%'],
+      '2xl': ['32px', '110%'],
+      '3xl': ['48px', '110%'],
     },
     container: {
       screens: {
