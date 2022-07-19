97b35f77e9d6b368758919d3facf4ae0466a3291
Luca Pipolo <luca@somoscuatro.es>
2022-07-19 10:57:29 +0200

    Fixed mobile devices layout was broken.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'auto-fill-100': 'repeat(auto-fill, minmax(140px, 1fr))',
+      },
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
